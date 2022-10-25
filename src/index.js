0355f2d3259748b521700168daf297c8c4f04613
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-10-24 17:23:44 -0700

    move code from index.js to pageLoad.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import './style.css'
-import FoodImage from './food-image.jpg';
 import { pageLoad } from './pageLoad'
 
 pageLoad()
-const content = document.getElementById("content");
 
-const myImage = new Image();
-myImage.classList = "image"
-myImage.src = FoodImage;
-
-content.appendChild(myImage);
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,5 +1,16 @@
+import './style.css'
+import FoodImage from './food-image.jpg';
+
 const pageLoad = () => {
   console.log('page load function')
+
+  const content = document.getElementById("content");
+
+  const myImage = new Image();
+  myImage.classList = "image"
+  myImage.src = FoodImage;
+
+  content.appendChild(myImage);
 }
 
 export { pageLoad };
