b6d94b2338b0cae5eeb854533ce353617baf64f1
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-10-25 12:25:50 -0700

    update contact info and rebundle

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,29 @@
 import './style.css'
 
 const contact = () => {
-  const nameCard = document.createElement('div')
-  nameCard.classList = "m-4 p-4 xl:w-1/3 border-4 border-sky-500"
-  content.appendChild(nameCard)
+  const content = document.getElementById("content")
+
+  const contactCard = document.createElement('div')
+  contactCard.classList = "m-4 p-4 xl:w-1/3 border-4 border-sky-500"
+  content.appendChild(contactCard)
+
+  const title = document.createElement('h1')
+  title.classList = "text-2xl text-sky-500 text-center"
+  title.textContent = "CONTACT US!"
+  title.tabIndex = 0
+  contactCard.appendChild(title)
+
+  const phone = document.createElement('h1')
+  phone.classList = "text-lg text-sky-500 text-center"
+  phone.textContent = "Phone Number: 559-222-0555"
+  phone.tabIndex = 0
+  contactCard.appendChild(phone)
+
+  const email = document.createElement('h1')
+  email.classList = "text-lg text-sky-500 text-center"
+  email.textContent = "Email: laboufresno@gmail.com"
+  email.tabIndex = 0
+  contactCard.appendChild(email)
 }
 
 export { contact }
